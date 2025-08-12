5ca2b0531d84be6d553f5348b022486df8df92d3
DelwinPrakash <delwinprakash499@gmail.com>
2025-08-12 23:57:16 +0530

    journel initialization

--- anchor/programs/crud/src/lib.rs
+++ anchor/programs/crud/src/lib.rs
@@ -8,63 +8,34 @@ declare_id!("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
 pub mod crud {
     use super::*;
 
-    pub fn close(_ctx: Context<CloseCrud>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn decrement(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.crud.count = ctx.accounts.crud.count.checked_sub(1).unwrap();
-        Ok(())
-    }
-
-    pub fn increment(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.crud.count = ctx.accounts.crud.count.checked_add(1).unwrap();
-        Ok(())
-    }
-
-    pub fn initialize(_ctx: Context<InitializeCrud>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-        ctx.accounts.crud.count = value.clone();
+    pub fn initializeJounel(ctx: Context<CreateJournelEntry>, title: String) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct InitializeCrud<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
+#[instruction(title: String)]
+pub struct CreateJournelEntry<'info>{
     #[account(
-  init,
-  space = 8 + Crud::INIT_SPACE,
-  payer = payer
+        init,
+        payer = owner,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        space = 8 + JornelEntry::INIT_SPACE,
     )]
-    pub crud: Account<'info, Crud>,
-    pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCrud<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
+    pub journel_entry: Account<'info, JornelEntry>,
 
-    #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-    )]
-    pub crud: Account<'info, Crud>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
     #[account(mut)]
-    pub crud: Account<'info, Crud>,
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Crud {
-    count: u8,
+pub struct JornelEntry {
+    pub owner: Pubkey,
+    #[max_len(20)]
+    pub title: String,
+    #[max_len(500)]
+    pub content: String
 }
