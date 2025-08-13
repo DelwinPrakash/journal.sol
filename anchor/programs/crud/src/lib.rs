0f0b2d9effd5268069b9442a60867c21329b9be8
DelwinPrakash <delwinprakash499@gmail.com>
2025-08-13 19:15:29 +0530

    feat: update journal entry

--- anchor/programs/crud/src/lib.rs
+++ anchor/programs/crud/src/lib.rs
@@ -1,41 +1,75 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-
 declare_id!("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
 
 #[program]
 pub mod crud {
     use super::*;
 
-    pub fn initializeJounel(ctx: Context<CreateJournelEntry>, title: String) -> Result<()> {
+    pub fn initializeJournal(ctx: Context<CreateJournalEntry>, title: String, content: String) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+
+        journal_entry.owner = ctx.accounts.owner.key();
+        journal_entry.title = title;
+        journal_entry.content = content;
+        
+        Ok(())
+    }
+
+    pub fn updateJournalEntry(ctx: Context<UpdateJournalEntry>, content: String, _title: String) -> Result<()>{
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.content = content;
+        
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 #[instruction(title: String)]
-pub struct CreateJournelEntry<'info>{
+pub struct CreateJournalEntry<'info>{
     #[account(
         init,
         payer = owner,
         seeds = [title.as_bytes(), owner.key().as_ref()],
         bump,
-        space = 8 + JornelEntry::INIT_SPACE,
+        space = 8 + JournalEntry::INIT_SPACE,
     )]
-    pub journel_entry: Account<'info, JornelEntry>,
+    pub journal_entry: Account<'info, JournalEntry>,
 
     #[account(mut)]
     pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateJournalEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + JournalEntry::INIT_SPACE,
+        realloc::payer = owner,
+        realloc::zero = true,
+    )]
+    pub journal_entry: Account<'info, JournalEntry>,
+    
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
     pub system_program: Program<'info, System>
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct JornelEntry {
+pub struct JournalEntry {
     pub owner: Pubkey,
+ 
     #[max_len(20)]
     pub title: String,
+ 
     #[max_len(500)]
     pub content: String
 }
