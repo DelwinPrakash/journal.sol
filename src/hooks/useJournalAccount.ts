1ce677b0ba90e4eb314d5d09c3a674147107df3a
DelwinPrakash <delwinprakash499@gmail.com>
2025-09-01 13:35:26 +0530

    journal account fetching data imple

---
+++ src/hooks/useJournalAccount.ts
@@ -0,0 +1,29 @@
+import { useWorkSpace } from "@/lib/anchorClient";
+import { PublicKey } from "@solana/web3.js";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useJournalAccount(){
+    const { program, wallet } = useWorkSpace();
+
+    const getAllJournals = useQuery({
+        queryKey: ["journal", "all"],
+        queryFn: () => program?.account.journalEntry.all(),
+        enabled: !!program && !!wallet?.publicKey
+    });
+
+    const getUserJournals = useQuery({
+            queryKey: ["journal", "user"],
+            queryFn: () => program?.account.journalEntry.all([{
+                memcmp:{
+                    offset: 8,
+                    bytes: wallet?.publicKey?.toBase58()!
+                }
+            }]),
+            enabled: !!program && !!wallet?.publicKey
+        });
+
+    return {
+        getAllJournals,
+        getUserJournals
+    }
+}
