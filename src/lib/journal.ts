39cb26e44c4d2f4a5f108b56f41985cb7ac1df4d
DelwinPrakash <delwinprakash499@gmail.com>
2025-09-10 18:56:47 +0530

    removed pda functions

--- src/lib/journal.ts
+++ src/lib/journal.ts
@@ -1,41 +1,15 @@
-import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { PROGRAM_ID } from "@/utils/constants";
 import { WalletContextState } from "@solana/wallet-adapter-react";
 import { Program } from "@coral-xyz/anchor";
+import type { Crud } from "@/utils/types/crud";
 
-function getJournalPDA(title: string, owner: PublicKey){
-    return PublicKey.findProgramAddressSync(
-        [Buffer.from(title), owner.toBuffer()],
-        PROGRAM_ID
-    );
-}
-
-export async function createJournal(title: string, content: string, isPublic: boolean, program: Program) {
+export async function createJournal(title: string, content: string, isPublic: boolean, program: Program<Crud>) {
     await program?.methods.initializeJournal(title, content, isPublic).rpc();
 }
 
-export async function updateJournal(title: string, content: string, program: Program , wallet: WalletContextState){
-    const [journalPDA] = getJournalPDA(title, wallet.publicKey!);
-
-    await program?.methods
-        .updateJournalEntry(title, content)
-        .accounts({
-            journalEntry: journalPDA,
-            owner: wallet.publicKey!,
-            systemProgram: SystemProgram.programId
-        })
-        .rpc();
+export async function updateJournal(title: string, content: string, program: Program<Crud>){
+    await program?.methods.updateJournalEntry(title, content).rpc();
 }
 
-export async function deleteJournal(title: string, program: Program, wallet: WalletContextState){
-    const [journalPDA] = getJournalPDA(title, wallet.publicKey!);
-
-    await program?.methods
-        .deleteJournalEntry(title)
-        .accounts({
-            journalEntry: journalPDA,
-            owner: wallet.publicKey!,
-            systemProgram: SystemProgram.programId
-        })
-        .rpc();
+export async function deleteJournal(title: string, program: Program<Crud>){
+    await program?.methods.deleteJournalEntry(title).rpc();
 }
